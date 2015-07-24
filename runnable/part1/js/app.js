f3494e3836b6cc0534b063e97a081d4490198e17
Kevin Harbal <kevin.harbal@fr.ibm.com>
2015-07-24 17:18:35 +0200

    rename store property

--- runnable/part1/js/app.js
+++ runnable/part1/js/app.js
@@ -1,11 +1,10 @@
 define([
 	"delite/register", "delite/theme!delite/themes/{{theme}}/global.css", "deliteful/ViewStack",
 	"deliteful/SidePane", "deliteful/LinearLayout", "deliteful/Button", "deliteful/StarRating",
-	"deliteful/ProgressBar", "deliteful/Store", "deliteful/list/List", "requirejs-domready/domReady!"
+	"deliteful/ProgressBar", "deliteful/list/List", "requirejs-domready/domReady!"
 ], function (register) {
 	register.parse();
 	document.body.style.display = "";
-	list.selectedItem = list.store.get("first");
 
 	/* app code will go here */
 
