5225638c74672916e07f580eda34a9ebdca4b034
Kevin Harbal <kevin.harbal@fr.ibm.com>
2015-07-24 13:39:17 +0200

    rename store property + delete deliteful/Store.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 define([
 	"delite/theme!delite/themes/{{theme}}/global.css", "deliteful/ViewStack",
 	"deliteful/SidePane", "deliteful/LinearLayout", "deliteful/Button", "deliteful/StarRating",
-	"deliteful/ProgressBar", "deliteful/Store", "deliteful/list/List",
+	"deliteful/ProgressBar", "deliteful/list/List",
 	"requirejs-dplugins/css!Font-Awesome/css/font-awesome.css", "requirejs-domready/domReady!"
 ], function () {
 	document.body.style.display = "";
-	list.selectedItem = list.store.get("first");
 
 	/* app code will go here */
 
